b1f48b49c1aa6af834ec27423b3f2437f07e720f
Suraj Chaudhary <chaudharysuraj477@gmail.com>
2025-09-12 12:54:12 +0100

    title pushed

--- app/publications/[slug]/page.tsx
+++ app/publications/[slug]/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata({ params }: ArticlePageProps) {
 
   if (!article) {
     return {
-      title: "Article Not Found",
+      title: "Portfolio",
     }
   }
 
