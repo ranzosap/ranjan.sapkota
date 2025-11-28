6db82d8f1ec85b5568de655538789aa32f1ef819
Suraj Chaudhary <chaudharysuraj477@gmail.com>
2025-11-28 12:33:38 +0000

    css pushed

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -26,7 +26,7 @@ export function MdxContent({ children, content, code }: MdxContentProps) {
         if (heightMatch) style += `height:${heightMatch[1]};`
       }
       if (isVideo) {
-        return `<div style="position:relative;width:100%;max-width:100%;aspect-ratio:16/9;"><video src="${src}" controls preload="none" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:cover;"></video></div>`
+        return `<div style="position:relative;width:100%;max-width:100%;aspect-ratio:16/9;"><video src="${src}" controls preload="none" style="position:absolute;top:0;left:0;width:100%;height:100%;object-fit:contain;"></video></div>`
       }
       if (isAudio) {
         return `<audio src="${src}" controls style="width:100%"></audio>`
